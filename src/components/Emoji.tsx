ababa6191b330e20dbed360968664b412732634a
arben-grepi <arben.grepi@gmail.com>
2024-09-23 23:46:34 +0300

    made the emojis smaller

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -10,9 +10,9 @@ interface Props {
 export const Emoji = ({ rating }: Props) => {
   if (rating < 3) return null;
   const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: "meh", boxSize: "25px" },
-    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
-    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+    3: { src: meh, alt: "meh", boxSize: "15px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "15px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "20px" },
   };
   return <Image {...emojiMap[rating]} marginTop={1} />;
 };
